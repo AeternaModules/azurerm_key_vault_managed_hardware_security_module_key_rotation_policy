eff68ec174c10045b07151627dc28c1cdb822da7
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "key_vault_managed_hardware_security_module_key_rotation_policies" {
   description = "All key_vault_managed_hardware_security_module_key_rotation_policy resources"
   value       = azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies
 }
+output "key_vault_managed_hardware_security_module_key_rotation_policies_expire_after" {
+  description = "List of expire_after values across all key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.expire_after]
+}
+output "key_vault_managed_hardware_security_module_key_rotation_policies_managed_hsm_key_id" {
+  description = "List of managed_hsm_key_id values across all key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.managed_hsm_key_id]
+}
+output "key_vault_managed_hardware_security_module_key_rotation_policies_time_after_creation" {
+  description = "List of time_after_creation values across all key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.time_after_creation]
+}
+output "key_vault_managed_hardware_security_module_key_rotation_policies_time_before_expiry" {
+  description = "List of time_before_expiry values across all key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.time_before_expiry]
+}
 
