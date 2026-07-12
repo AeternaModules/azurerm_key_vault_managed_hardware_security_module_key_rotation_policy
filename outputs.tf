baa6b79bea2090e95b5263012a210a327d960cf1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "key_vault_managed_hardware_security_module_key_rotation_policies" {
-  description = "All key_vault_managed_hardware_security_module_key_rotation_policy resources"
-  value       = azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies
-}
 output "key_vault_managed_hardware_security_module_key_rotation_policies_expire_after" {
-  description = "List of expire_after values across all key_vault_managed_hardware_security_module_key_rotation_policies"
-  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.expire_after]
+  description = "Map of expire_after values across all key_vault_managed_hardware_security_module_key_rotation_policies, keyed the same as var.key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = { for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : k => v.expire_after }
 }
 output "key_vault_managed_hardware_security_module_key_rotation_policies_managed_hsm_key_id" {
-  description = "List of managed_hsm_key_id values across all key_vault_managed_hardware_security_module_key_rotation_policies"
-  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.managed_hsm_key_id]
+  description = "Map of managed_hsm_key_id values across all key_vault_managed_hardware_security_module_key_rotation_policies, keyed the same as var.key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = { for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : k => v.managed_hsm_key_id }
 }
 output "key_vault_managed_hardware_security_module_key_rotation_policies_time_after_creation" {
-  description = "List of time_after_creation values across all key_vault_managed_hardware_security_module_key_rotation_policies"
-  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.time_after_creation]
+  description = "Map of time_after_creation values across all key_vault_managed_hardware_security_module_key_rotation_policies, keyed the same as var.key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = { for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : k => v.time_after_creation }
 }
 output "key_vault_managed_hardware_security_module_key_rotation_policies_time_before_expiry" {
-  description = "List of time_before_expiry values across all key_vault_managed_hardware_security_module_key_rotation_policies"
-  value       = [for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : v.time_before_expiry]
+  description = "Map of time_before_expiry values across all key_vault_managed_hardware_security_module_key_rotation_policies, keyed the same as var.key_vault_managed_hardware_security_module_key_rotation_policies"
+  value       = { for k, v in azurerm_key_vault_managed_hardware_security_module_key_rotation_policy.key_vault_managed_hardware_security_module_key_rotation_policies : k => v.time_before_expiry }
 }
 
